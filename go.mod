d60a21c0854f207eaadb5344b8cc5f0d9499d48c
张声录 <zhangshenglu@idszt.com>
2025-08-20 02:20:44 +0800

    minio客户端工修改

--- go.mod
+++ go.mod
@@ -3,54 +3,34 @@ module minio-example
 go 1.24.2
 
 require (
-	github.com/IBM/sarama v1.45.2
 	github.com/minio/madmin-go/v4 v4.2.4
-	github.com/minio/minio-go/v7 v7.0.94
-	github.com/pkg/sftp v1.13.9
-	golang.org/x/crypto v0.39.0
-	gopkg.in/yaml.v2 v2.4.0
+	github.com/minio/minio-go/v7 v7.0.95
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/eapache/go-resiliency v1.7.0 // indirect
-	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
-	github.com/eapache/queue v1.1.0 // indirect
 	github.com/ebitengine/purego v0.8.4 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/hashicorp/go-uuid v1.0.3 // indirect
-	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
-	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
-	github.com/jcmturner/gofork v1.7.6 // indirect
-	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
-	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
-	github.com/kr/fs v0.1.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.11 // indirect
 	github.com/lufia/plan9stats v0.0.0-20250317134145-8bc96cf8fc35 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/minio/crc64nvme v1.0.2 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
-	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.65.0 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
 	github.com/prometheus/prom2json v1.4.2 // indirect
 	github.com/prometheus/prometheus v0.304.1 // indirect
-	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/safchain/ethtool v0.6.1 // indirect
 	github.com/secure-io/sio-go v0.3.1 // indirect
@@ -59,6 +39,7 @@ require (
 	github.com/tklauser/go-sysconf v0.3.15 // indirect
 	github.com/tklauser/numcpus v0.10.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
--- main.go
+++ main.go
@@ -2,479 +2,79 @@ package main
 
 import (
 	"context"
-	"flag"
-	"fmt"
-	"io"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"log"
-	"os"
-	"path/filepath"
-	"strings"
-	"sync"
-	"time"
-
-	"github.com/IBM/sarama"
-	"github.com/pkg/sftp"
-	"golang.org/x/crypto/ssh"
-	"gopkg.in/yaml.v2"
 )
 
-// Config 配置结构体
-type Config struct {
-	SFTPSources []SFTPConfig `yaml:"sftp_sources"`
-	SFTPTarget  SFTPConfig   `yaml:"sftp_target"`
-	Kafka       KafkaConfig  `yaml:"kafka"`
-	Interval    int          `yaml:"interval"` // 扫描间隔，秒
-}
-
-// SFTPConfig SFTP配置
-type SFTPConfig struct {
-	Host       string `yaml:"host"`
-	Port       int    `yaml:"port"`
-	Username   string `yaml:"username"`
-	Password   string `yaml:"password"`
-	InDir      string `yaml:"in_dir"`
-	OutDir     string `yaml:"out_dir"`
-	PrivateKey string `yaml:"private_key,omitempty"`
-}
-
-// KafkaConfig Kafka配置
-type KafkaConfig struct {
-	Brokers      []string `yaml:"brokers"`
-	ProduceTopic string   `yaml:"produce_topic"` // 生产者发送消息的主题
-	ConsumeTopic string   `yaml:"consume_topic"` // 消费者接收消息的主题
-	GroupID      string   `yaml:"group_id"`
-}
-
-// FileMessage 文件消息结构
-type FileMessage struct {
-	SourceHost string
-	Filename   string
-	Content    string
-	Timestamp  time.Time
-}
-
-// SFTPClient SFTP客户端封装
-type SFTPClient struct {
-	client *sftp.Client
-	conn   *ssh.Client
-	config SFTPConfig
-}
-
-// NewSFTPClient 创建SFTP客户端
-func NewSFTPClient(config SFTPConfig) (*SFTPClient, error) {
-	var authMethod ssh.AuthMethod
-	if config.PrivateKey != "" {
-		key, err := os.ReadFile(config.PrivateKey)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read private key: %v", err)
-		}
-		signer, err := ssh.ParsePrivateKey(key)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse private key: %v", err)
-		}
-		authMethod = ssh.PublicKeys(signer)
-	} else {
-		authMethod = ssh.Password(config.Password)
-	}
-
-	sshConfig := &ssh.ClientConfig{
-		User:            config.Username,
-		Auth:            []ssh.AuthMethod{authMethod},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		Timeout:         30 * time.Second,
-	}
-
-	addr := fmt.Sprintf("%s:%d", config.Host, config.Port)
-	conn, err := ssh.Dial("tcp", addr, sshConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to SSH server: %v", err)
-	}
-
-	client, err := sftp.NewClient(conn)
-	if err != nil {
-		conn.Close()
-		return nil, fmt.Errorf("failed to create SFTP client: %v", err)
-	}
-
-	return &SFTPClient{
-		client: client,
-		conn:   conn,
-		config: config,
-	}, nil
-}
-
-// Close 关闭连接
-func (s *SFTPClient) Close() error {
-	if s.client != nil {
-		s.client.Close()
-	}
-	if s.conn != nil {
-		return s.conn.Close()
-	}
-	return nil
-}
-
-// ReadFiles 读取指定目录下的.mh文件
-func (s *SFTPClient) ReadFiles() ([]FileMessage, error) {
-	inDir := s.config.InDir
-	if inDir == "" {
-		inDir = "/IN"
-	}
-
-	files, err := s.client.ReadDir(inDir)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read directory %s: %v", inDir, err)
-	}
-
-	var messages []FileMessage
-	for _, file := range files {
-		if file.IsDir() || !strings.HasSuffix(file.Name(), ".mh") {
-			continue
-		}
-
-		filePath := inDir + file.Name()
-		content, err := s.readFile(filePath)
-		if err != nil {
-			log.Printf("Failed to read file %s: %v", filePath, err)
-			continue
-		}
-
-		fmt.Printf("=== 读取文件信息 ===\n")
-		fmt.Printf("来源地址: %s:%d\n", s.config.Host, s.config.Port)
-		fmt.Printf("文件路径: %s\n", filePath)
-		fmt.Printf("文件名: %s\n", file.Name())
-		fmt.Printf("文件内容:\n%s\n", content)
-
-		message := FileMessage{
-			SourceHost: fmt.Sprintf("%s:%d", s.config.Host, s.config.Port),
-			Filename:   file.Name(),
-			Content:    content,
-			Timestamp:  time.Now(),
-		}
-
-		messages = append(messages, message)
-
-		// 删除已处理的文件
-		if err := s.client.Remove(filePath); err != nil {
-			log.Printf("Failed to delete file %s: %v", filePath, err)
-		} else {
-			fmt.Printf("已删除源文件: %s\n", filePath)
-		}
-	}
-
-	return messages, nil
-}
-
-// readFile 读取文件内容
-func (s *SFTPClient) readFile(filePath string) (string, error) {
-	file, err := s.client.Open(filePath)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	content, err := io.ReadAll(file)
-	if err != nil {
-		return "", err
-	}
-
-	return string(content), nil
-}
-
-// WriteFile 写入文件到目标目录
-func (s *SFTPClient) WriteFile(filename, content string) error {
-	outDir := s.config.OutDir
-	if outDir == "" {
-		outDir = "/OUT/"
-	}
-
-	// 确保目录存在
-	if err := s.client.MkdirAll(outDir); err != nil {
-		return fmt.Errorf("failed to create directory %s: %v", outDir, err)
-	}
-
-	filePath := outDir + filename
-	file, err := s.client.Create(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to create file %s: %v", filePath, err)
-	}
-	defer file.Close()
-
-	if _, err := file.Write([]byte(content)); err != nil {
-		return fmt.Errorf("failed to write file %s: %v", filePath, err)
-	}
-
-	fmt.Printf("=== 文件上传完成 ===\n")
-	fmt.Printf("目标地址: %s:%d\n", s.config.Host, s.config.Port)
-	fmt.Printf("上传路径: %s\n", filePath)
-	fmt.Printf("文件名: %s\n", filename)
-
-	return nil
-}
-
-// KafkaProducer Kafka生产者
-type KafkaProducer struct {
-	producer sarama.SyncProducer
-	topic    string
-}
-
-// NewKafkaProducer 创建Kafka生产者
-func NewKafkaProducer(config KafkaConfig) (*KafkaProducer, error) {
-	saramaConfig := sarama.NewConfig()
-	saramaConfig.Producer.Return.Successes = true
-	saramaConfig.Producer.RequiredAcks = sarama.WaitForAll
-	saramaConfig.Producer.Retry.Max = 5
-
-	producer, err := sarama.NewSyncProducer(config.Brokers, saramaConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create Kafka producer: %v", err)
-	}
-
-	return &KafkaProducer{
-		producer: producer,
-		topic:    config.ProduceTopic,
-	}, nil
-}
-
-// SendMessage 发送消息到Kafka
-func (k *KafkaProducer) SendMessage(message FileMessage) error {
-	fmt.Printf("=== 发送Kafka消息 ===\n")
-	fmt.Printf("主题: %s\n", k.topic)
-	fmt.Printf("文件名: %s\n", message.Filename)
-	fmt.Printf("来源: %s\n", message.SourceHost)
-	fmt.Printf("消息内容: %s\n", message.Content)
-
-	msg := &sarama.ProducerMessage{
-		Topic: k.topic,
-		Key:   sarama.StringEncoder(message.Filename), // 使用文件名作为消息key
-		Value: sarama.StringEncoder(message.Content),  // 直接发送文件内容
-	}
-
-	partition, offset, err := k.producer.SendMessage(msg)
-	if err != nil {
-		return fmt.Errorf("failed to send message to Kafka: %v", err)
-	}
-
-	fmt.Printf("消息发送成功 - 分区: %d, 偏移量: %d\n", partition, offset)
-	return nil
-}
-
-// Close 关闭生产者
-func (k *KafkaProducer) Close() error {
-	return k.producer.Close()
-}
-
-// KafkaConsumer Kafka消费者
-type KafkaConsumer struct {
-	consumer sarama.ConsumerGroup
-	topic    string
-	groupID  string
-}
-
-// NewKafkaConsumer 创建Kafka消费者
-func NewKafkaConsumer(config KafkaConfig) (*KafkaConsumer, error) {
-	saramaConfig := sarama.NewConfig()
-	saramaConfig.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
-	saramaConfig.Consumer.Offsets.Initial = sarama.OffsetOldest
-	saramaConfig.Consumer.Group.Session.Timeout = 10 * time.Second
-	saramaConfig.Consumer.Group.Heartbeat.Interval = 3 * time.Second
-
-	consumer, err := sarama.NewConsumerGroup(config.Brokers, config.GroupID, saramaConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create Kafka consumer: %v", err)
-	}
-
-	return &KafkaConsumer{
-		consumer: consumer,
-		topic:    config.ConsumeTopic,
-		groupID:  config.GroupID,
-	}, nil
-}
-
-// ConsumerGroupHandler 消费者组处理器
-type ConsumerGroupHandler struct {
-	sftpClient *SFTPClient
-}
-
-// Setup 设置消费者
-func (h *ConsumerGroupHandler) Setup(sarama.ConsumerGroupSession) error {
-	return nil
-}
-
-// Cleanup 清理消费者
-func (h *ConsumerGroupHandler) Cleanup(sarama.ConsumerGroupSession) error {
-	return nil
-}
-
-// ConsumeClaim 消费消息
-func (h *ConsumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	for {
-		select {
-		case message := <-claim.Messages():
-			if message == nil {
-				return nil
-			}
-
-			fmt.Printf("=== 接收到Kafka消息 ===\n")
-			fmt.Printf("分区: %d, 偏移量: %d\n", message.Partition, message.Offset)
-
-			// 从消息key获取文件名，如果没有key则生成默认文件名
-			filename := string(message.Key)
-			if filename == "" {
-				filename = fmt.Sprintf("file_%d_%d.mh", message.Partition, message.Offset)
-			}
-
-			content := string(message.Value)
-			fmt.Printf("文件名: %s\n", filename)
-			fmt.Printf("消息内容: %s\n", content)
-
-			// 写入文件到SFTP
-			if err := h.sftpClient.WriteFile(filename, content); err != nil {
-				log.Printf("Failed to write file to SFTP: %v", err)
-				continue
-			}
-
-			session.MarkMessage(message, "")
-
-		case <-session.Context().Done():
-			return nil
-		}
-	}
-}
-
-// Consume 开始消费
-func (k *KafkaConsumer) Consume(ctx context.Context, sftpClient *SFTPClient) error {
-	handler := &ConsumerGroupHandler{sftpClient: sftpClient}
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
-			if err := k.consumer.Consume(ctx, []string{k.topic}, handler); err != nil {
-				log.Printf("Error consuming messages: %v", err)
-				time.Sleep(5 * time.Second)
-			}
-		}
-	}
-}
-
-// Close 关闭消费者
-func (k *KafkaConsumer) Close() error {
-	return k.consumer.Close()
-}
-
-// loadConfig 加载配置文件
-func loadConfig(configFile string) (*Config, error) {
-	data, err := os.ReadFile(configFile)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read config file: %v", err)
-	}
-
-	var config Config
-	if err := yaml.Unmarshal(data, &config); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal config: %v", err)
-	}
-
-	// 设置默认值
-	if config.Interval == 0 {
-		config.Interval = 10
-	}
-
-	return &config, nil
-}
-
-func startFtpKafkaTask() {
-	var configFile string
-	flag.StringVar(&configFile, "config", "config.yaml", "配置文件路径")
-	flag.Parse()
-
-	// 加载配置
-	config, err := loadConfig(configFile)
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	fmt.Printf("=== 启动SFTP-Kafka文件传输工具 ===\n")
-	fmt.Printf("配置文件: %s\n", configFile)
-	fmt.Printf("扫描间隔: %d秒\n", config.Interval)
-
-	// 创建Kafka生产者
-	producer, err := NewKafkaProducer(config.Kafka)
-	if err != nil {
-		log.Fatalf("Failed to create Kafka producer: %v", err)
-	}
-	defer producer.Close()
-
-	// 创建目标SFTP客户端
-	targetSFTP, err := NewSFTPClient(config.SFTPTarget)
-	if err != nil {
-		log.Fatalf("Failed to create target SFTP client: %v", err)
-	}
-	defer targetSFTP.Close()
-
-	// 创建Kafka消费者
-	consumer, err := NewKafkaConsumer(config.Kafka)
-	if err != nil {
-		log.Fatalf("Failed to create Kafka consumer: %v", err)
-	}
-	defer consumer.Close()
-
-	// 启动消费者协程
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		if err := consumer.Consume(ctx, targetSFTP); err != nil {
-			log.Printf("Consumer error: %v", err)
-		}
-	}()
-
-	// 定期扫描源SFTP目录
-	ticker := time.NewTicker(time.Duration(config.Interval) * time.Second)
-	defer ticker.Stop()
-	filepath.Join()
-	for {
-		select {
-		case <-ticker.C:
-			fmt.Printf("\n=== 开始扫描源SFTP目录 ===\n")
-			for i, sftpConfig := range config.SFTPSources {
-				fmt.Printf("扫描源 %d: %s:%d\n", i+1, sftpConfig.Host, sftpConfig.Port)
-
-				client, err := NewSFTPClient(sftpConfig)
-				if err != nil {
-					log.Printf("Failed to create SFTP client for %s:%d: %v",
-						sftpConfig.Host, sftpConfig.Port, err)
-					continue
-				}
-
-				messages, err := client.ReadFiles()
-				client.Close()
-
-				if err != nil {
-					log.Printf("Failed to read files from %s:%d: %v",
-						sftpConfig.Host, sftpConfig.Port, err)
-					continue
-				}
-
-				// 发送消息到Kafka
-				for _, message := range messages {
-					if err := producer.SendMessage(message); err != nil {
-						log.Printf("Failed to send message to Kafka: %v", err)
-					}
-				}
-			}
-		case <-ctx.Done():
-			wg.Wait()
-			return
-		}
-	}
-
-}
-
 func main() {
-	startFtpKafkaTask()
+	endpoint := "47.98.132.219:9000"
+	accessKeyID := "8OTHJPNJOAYLX6S9RQB9"
+	secretAccessKey := "Wc5dMy1UKIEcHpHMWpOiFL4FfZ6Az9Tx7njcqPrl"
+	useSSL := false
+	client, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln("初始minio客户端失败", err)
+	}
+	log.Println("初始minio客户端成功")
+	// create bucket
+	bucketName := "test-bucket-go"
+
+	// check bucket exists
+	exists, err := client.BucketExists(context.Background(), bucketName)
+	if err != nil {
+		log.Fatalln("检查bucket是否存在失败", err)
+	}
+	if exists {
+		log.Println("bucket已存在")
+	} else {
+		// create bucket
+		err := client.MakeBucket(context.Background(), bucketName, minio.MakeBucketOptions{})
+		if err != nil {
+			log.Fatalln("创建bucket失败", err)
+		}
+		log.Println("创建bucket成功")
+	}
+	// upload object
+	fileName := "1.txt"
+	objectName := "go/1.txt"
+	object, err := client.FPutObject(context.Background(), bucketName, objectName, fileName, minio.PutObjectOptions{})
+	if err != nil {
+		return
+	}
+	if err != nil {
+		log.Fatalln("上传文件失败", err)
+	}
+	log.Println("上传文件成功", object)
+
+	// download object
+	err = client.FGetObject(context.Background(), bucketName, objectName, "down.txt", minio.GetObjectOptions{})
+	if err != nil {
+		log.Fatalln("下载文件失败", err)
+	}
+
+	// list object
+	objects := client.ListObjects(context.Background(), bucketName, minio.ListObjectsOptions{
+		Prefix:    "go/",
+		Recursive: true,
+	})
+	for obj := range objects {
+		log.Println("文件名", obj.Key, "大小", obj.Size, "修改时间", obj.LastModified, "元数据", obj.UserMetadata, "版本ID", obj.VersionID)
+	}
+	// delete object
+	err = client.RemoveObject(context.Background(), bucketName, objectName, minio.RemoveObjectOptions{})
+	if err != nil {
+		log.Fatalln("删除文件失败", err)
+	}
+	log.Println("删除文件成功")
+
+	// delete bucket
+	err = client.RemoveBucket(context.Background(), bucketName)
+	if err != nil {
+		log.Fatalln("删除bucket失败", err)
+	}
+	log.Println("删除bucket成功")
 }
